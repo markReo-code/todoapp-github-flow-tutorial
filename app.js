a1a9ff05e7d344c70c2a79eaaeafbe5b5c1ee8d5
markReo-code <youyilangtutian@gmail.com>
2023-11-25 10:52:17 +0900

    add function todo-add done

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ $("#addTodo").click(function () {
     const inputTodo = $("input").val();
     $("#todoList").append("<li><input type='checkbox'>" + inputTodo + "</li>");
 
-    $("input").val(""); //挿入し終わったら空にしておく
+    $("input").val(""); //挿入し終わったら空にし
 })
